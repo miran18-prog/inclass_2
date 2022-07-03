eb607c0a38dca7b5a2390c6c60c302f1535a1a31
Miran Amanj <miran.am2001@gmail.com>
2022-07-03 16:32:22 +0300

    first commit

---
+++ lib/screens/screen_view.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FutureScreenView extends StatelessWidget {
+  const FutureScreenView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Futures"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: FutureBuilder(
+          future: waitForFiveSec(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else {
+              return Text("Done");
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  waitForFiveSec() async {
+    return await Future.delayed(Duration(seconds: 4)).then((value) => true);
+  }
+}
